2396adfa54be74426fb3530e07ae648d0c36f548
root <root@hks-webb.hultsfred.se>
2013-01-28 22:03:29 +0100

    style fixes

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function hk_get_tele_search($host, $user, $pwd, $db, $search, $num_hits = -1) {
 function hk_pre_ajax_search_function($search) {
 	$options = get_option("hk_theme");
 	
-	$hits = hk_get_tele_search($options["tele_db_host"], $options["tele_db_user"], $options["tele_db_pwd"], $options["tele_db_db"], $search, 5);
+	$hits = hk_get_tele_search($options["tele_db_host"], $options["tele_db_user"], $options["tele_db_pwd"], $options["tele_db_db"], $search, 3);
 	
 	// echo if hits found
 	if (count($hits) > 0) :
